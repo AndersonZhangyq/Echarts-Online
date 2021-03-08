792aedccfd1a94bb0ac51fe5e7c7948165f31727
AndersonZhangyq <andersonzhangyq@gmail.com>
2021-03-08 09:54:33 +0800

    add luckysheet

--- src/pages/Data.vue
+++ src/pages/Data.vue
@@ -1,8 +1,10 @@
 <template>
-  <div
-    id="luckysheet"
-    style="margin:0px;padding:0px;position:absolute;width:100%;left: 0px;top: 30px;bottom:0px;"
-  ></div>
+  <q-page>
+    <div
+      id="luckysheet"
+      style="position: absolute;width: 100%;left: 0px;top: 0px;bottom: 0px;right: 0px"
+    ></div>
+  </q-page>
 </template>
 
 <script>
@@ -12,6 +14,29 @@ export default {
     return {
       leftDrawerOpen: false
     };
+  },
+  mounted() {
+    // In some cases, you need to use $nextTick
+    // this.$nextTick(() => {
+    luckysheet.create({
+      container: "luckysheet", // 设定DOM容器的id
+      title: "Luckysheet Demo", // 设定表格名称
+      lang: "zh", // 设定表格语言
+      plugins: ["chart"],
+      showtoolbar: false,
+      showtoolbarConfig: {
+        undoRedo: true,
+        paintFormat: true, //格式刷
+        numberDecrease: true, // '减少小数位数'
+        numberIncrease: true, // '增加小数位数
+        font: true, // '字体'
+        fontSize: true, // '字号大小'
+        bold: true, // '粗体 (Ctrl+B)'
+        italic: true // '斜体 (Ctrl+I)'
+      }
+    });
+
+    // });
   }
 };
 </script>
